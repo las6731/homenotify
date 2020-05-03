329a788bdf9b6cfd9ab122dcc95644aecc3f0e0c
Lukas Short <las6731@rit.edu>
2020-05-03 10:56:15 -0400

    set up basic Firebase android app
    TODO: allow user to define app server, and request a list of topics available to subscribe to
    TODO: use message data to build notifications (custom actions, etc)

---
+++ HomeNotify.Android/FirebaseMessagingServiceImpl.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using Android.App;
+using Android.Content;
+using Android.Support.V4.App;
+using Android.Util;
+using Firebase.Messaging;
+
+namespace HomeNotify.Android
+{
+    [Service(Name = "com.lshort.homenotify.FirebaseMessagingServiceImpl")]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    public class FirebaseMessagingServiceImpl : FirebaseMessagingService
+    {
+        private const string TAG = "FirebaseMessagingServiceImpl";
+        public override void OnNewToken(string token)
+        {
+            Log.Debug(TAG, "Refreshed token: " + token);
+        }
+
+        public override void OnMessageReceived(RemoteMessage message)
+        {
+            SendNotification(message.GetNotification().Body, message.Data);
+        }
+        
+        void SendNotification(string messageBody, IDictionary<string, string> data)
+        {
+            var intent = new Intent(this, typeof(MainActivity));
+            intent.AddFlags(ActivityFlags.ClearTop);
+            foreach (var key in data.Keys)
+            {
+                intent.PutExtra(key, data[key]);
+            }
+
+            var pendingIntent = PendingIntent.GetActivity(this,
+                MainActivity.NOTIFICATION_ID,
+                intent,
+                PendingIntentFlags.OneShot);
+
+            var notificationBuilder = new NotificationCompat.Builder(this, MainActivity.CHANNEL_ID)
+                .SetSmallIcon(Resource.Color.transparent)
+                .SetContentTitle("HomeNotify")
+                .SetContentText(messageBody)
+                .SetAutoCancel(true)
+                .SetContentIntent(pendingIntent);
+            
+            /*
+                TODO: use message data to make actions, etc
+                intention is to use ONLY data payloads so all notifications are built manually
+                (notification payloads don't pass through OnMessageReceived when app is in background)
+            */
+
+            var notificationManager = NotificationManagerCompat.From(this);
+            notificationManager.Notify(MainActivity.NOTIFICATION_ID, notificationBuilder.Build());
+        }
+    }
+}
--- HomeNotify.Android/MainActivity.cs
+++ HomeNotify.Android/MainActivity.cs
@@ -3,17 +3,72 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using Android.Content;
+using Android.Gms.Common;
+using Firebase.Messaging;
+using Firebase.Iid;
 
 namespace HomeNotify.Android
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        internal static readonly string CHANNEL_ID = "general_notification_channel";
+        internal static readonly int NOTIFICATION_ID = 100;
+
+        private TextView msgText;
+        protected override void OnCreate (Bundle bundle)
         {
-            base.OnCreate(savedInstanceState);
-            // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_main);
+            base.OnCreate (bundle);
+            SetContentView (Resource.Layout.activity_main);
+            msgText = FindViewById<TextView> (Resource.Id.msgText);
+
+            IsPlayServicesAvailable ();
+
+            CreateNotificationChannel();
+        }
+        
+        public bool IsPlayServicesAvailable ()
+        {
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable (this);
+            if (resultCode != ConnectionResult.Success)
+            {
+                if (GoogleApiAvailability.Instance.IsUserResolvableError (resultCode))
+                    msgText.Text = GoogleApiAvailability.Instance.GetErrorString (resultCode);
+                else
+                {
+                    msgText.Text = "This device is not supported";
+                    Finish ();
+                }
+                return false;
+            }
+            else
+            {
+                msgText.Text = "Google Play Services is available.";
+                return true;
+            }
+        }
+        
+        void CreateNotificationChannel()
+        {
+            if (Build.VERSION.SdkInt < BuildVersionCodes.O)
+            {
+                // Notification channels are new in API 26 (and not a part of the
+                // support library). There is no need to create a notification
+                // channel on older versions of Android.
+                return;
+            }
+
+            var channel = new NotificationChannel(CHANNEL_ID,
+                "FCM Notifications",
+                NotificationImportance.Default)
+            {
+
+                Description = "Firebase Cloud Messages appear in this channel"
+            };
+
+            var notificationManager = (NotificationManager)GetSystemService(NotificationService);
+            notificationManager.CreateNotificationChannel(channel);
         }
     }
 }
